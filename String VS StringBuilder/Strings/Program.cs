862a4f9d64787d15129546af4694e591d625d666
Zavier <zmyles@comcast.net>
2019-08-07 18:55:23 -0400

    Strings and StringBuilder

---
+++ String VS StringBuilder/Strings/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Robot story time...(Concatination)====================================
+            string myName = "Tom Curise";
+            string myPurpose = "To make awesome movies";
+            string myHomeworld = "Mars...I Mean, Earth! Just like the rest of us humans!";
+
+            Console.WriteLine("Greetings Earthling, I'm " + myName + ". My Goals are simply... I just want to " + myPurpose + ".");
+            Console.WriteLine("Orignally, I was born in "+ myHomeworld);
+            Console.ReadLine();
+
+            //Convert strings to UPPERCASE ====================================
+            Console.WriteLine("Can you give me a name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine(name.ToUpper());
+
+            //StringBuilder, What does it really do?=================================
+
+            StringBuilder sb = new StringBuilder("Test my stringbuilder", 50);
+            sb.Append("TEST IS IT WORKING?!");
+
+            Console.WriteLine(sb);
+
+
+
+
+        }
+    }
+}
