7b2bc05e8425c3c6f9ca8693878b85c9bd4d8a28
Zavier <zmyles@comcast.net>
2019-08-08 15:17:52 -0400

    Another C# exercise

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // PART 1=================================================
+            //=======================================================
+
+            //string[] phrases = { "Is cool", "Doesn't like brownies", "Is not a fan of heights" };
+
+            //Console.WriteLine("Tell me you're name");
+            //string name = Console.ReadLine();
+
+            //for (int i = 0; i < phrases.Length; i++)
+            //{
+            //    Console.WriteLine(name + " " + phrases[i]);
+            //}
+            //Console.ReadLine();
+
+            // PART 2 ================================================
+            //========================================================
+
+            //INIFINTE LOOP!!
+
+            //for ( int a = 0; a < 50; a--)
+            //{
+            //    Console.WriteLine("value : {0}", a);
+            //}
+            //Console.ReadLine();
+
+
+
+            //PART 3 =================================================
+            //==========================================================
+
+            //THE FIX...
+
+            //for (int a = 0; a < 50; a++)//Now it will add up and end
+            //{
+            //    Console.WriteLine("value : {0}", a);
+            //}
+            //Console.ReadLine();
+
+            //PART 4 ====================================================
+            //============================================================
+
+            //for (int j = 1; j < 50; j++){
+            //    Console.WriteLine(j);
+            //}
+
+            // PART 5 ======================================================
+            //==============================================================
+
+            //for (int k = 1; k <= 50; k++)
+            //{
+            //    Console.WriteLine(k);
+            //}
+
+            //PART 6 =======================================================
+            //=========================================================
+
+            //List<string> names = new List<string>() { "Jesse", "Erik", "Adam", "Daniel" };
+
+            //foreach(string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+            //Console.ReadLine();
+
+            List<string> animalList = new List<string>() { "Lions", "Tigers", "Bears", "Alligators" };
+
+            foreach(string beast in animalList)
+            {
+                Console.WriteLine(beast);
+            }
+
+            Console.WriteLine("Are you a fan of Fine furs or Scaly Skin? ")
+
+        }
+    }
+}
---
+++ Iteration/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] testScores = { 98, 99, 85, 70, 82, 34, 90, 91, 94 };
+
+            //for (int i = 0; i < testScores.Length; i++)
+            //{
+            //    if (testScores[i] > 85)
+            //    {
+            //        Console.WriteLine("Passing test score: " + testScores[i]);
+            //    }
+            //}
+            //Console.ReadLine();
+
+
+            //======================================================
+            //Strings !!!!!!!!!!!!!!!!!!!!!!!!!
+            //======================================================
+
+            //string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+
+            //for(int j = 0; j < names.Length; j++)
+            //{
+            //    Console.WriteLine(names[j]);
+            //}
+            //Console.ReadLine();
+
+
+
+            //+++++++++++++++++++++++++++++++++++++++++++++++++
+            //LIST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            //+++++++++++++++++++++++++++++++++++++++++++++++++
+            //List<int> testScores = new List<int>();
+            //testScores.Add(98);
+            //testScores.Add(18);
+            //testScores.Add(80);
+            //testScores.Add(100);
+            //testScores.Add(98);
+
+            //foreach(int score in testScores) {
+            //    if (score > 85)
+            //    {
+            //        Console.WriteLine("Passing test score:" + score);
+            //    }
+
+            //}
+            //Console.ReadLine();
+
+
+            //=================================================================
+
+            //List<string> names = new List<string>() { "Jesse", "Erik", "Adam", "Daniel" };
+
+            //foreach(string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+            //Console.ReadLine();
+
+
+
+            //=================================================================
+            //HOW TO TRANSFER DATA FROM ONE LIST TO ANOTHER!!!!!!!!!!
+            //=================================================================
+
+            List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 };
+            List<int> passingScores = new List<int>();
+
+            foreach(int score in testScores)
+            {
+                if(score > 85)
+                {
+                    passingScores.Add(score);
+                }
+            }
+
+            Console.WriteLine(passingScores.Count);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
