1e11f17348520d968d9137113e9c5595c7aeaf24
Zavier <zmyles@comcast.net>
2019-08-09 17:48:17 -0400

    Creating Methods and using them across a simply program.

---
+++ WorkingWithClases/ConsoleApp_Test/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_Test
+{
+    class MathOp
+    {
+        public static int Mutiply(int j)
+        {
+            int result = 2 * j;
+                return result;
+        }
+
+        public static int Modules(int k)
+        {
+            int result = 100 % k;
+            return result;
+        }
+
+        public static int Subtract(int s)
+        {
+            int result = 100 - s;
+            return result;
+        }
+
+
+
+        // TEST CODE+++++++++++++++++++++++
+        //public static int Add(int i)
+        //{
+        //    int num = 2 + i;
+        //    return num;
+        //}
+       
+
+    }
+}
---
+++ WorkingWithClases/ConsoleApp_Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What number would you like to multipy with 2?");
+            int userInput = MathOp.Mutiply(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("The number was: " + userInput);
+
+
+
+
+            Console.WriteLine("What number would you like to find the remainder of from 100?");
+            int userInput2 = MathOp.Modules(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("The number was: " + userInput2);
+
+
+
+            Console.WriteLine("What number would you like to subtract 100 from?");
+            int userInput3 = MathOp.Subtract(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("The number was: " + userInput3);
+
+
+            // TEST CODE=================
+            //int answer = MathOp.Add(10);
+            //Console.WriteLine("Our number is: " + answer);
+
+
+        }
+    }
+    }
