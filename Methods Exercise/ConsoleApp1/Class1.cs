1b6d19e47db04d1ee0b253d7ed4fba413fe38ed7
Zavier <zmyles@comcast.net>
2019-08-10 16:45:39 -0400

    OneMethod, Onepower, used one method for more then one use

---
+++ Methods Exercise/ConsoleApp1/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+
+    //TAKES TWO USER INPUTS INTO ONE METHOD =======
+    class Class1
+    {
+        //Multipy ========================================
+        public static int AddMethod(int x, int y)
+        {
+            int result = x * y;
+            return result;
+        }
+
+        //    //Used for decimals ======================================
+        public static int AddMethod(decimal x, decimal y)
+        {
+
+            int result2 = Convert.ToInt32(x + y);
+            return result2;
+
+        }
+        //Strings to int===============================
+        public static int AddMethod(string x, string y)
+        {
+             
+            int s1 = Convert.ToInt32(x);
+            int s2 = Convert.ToInt32(y);
+
+            int module = s1 % s2;
+
+            return module;
+
+        }
+
+    }
+}
---
+++ Methods Exercise/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace OneMethodOnePower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //TAKES TWO USER INPUTS INTO ONE METHOD =======
+            Console.WriteLine("Give me a number...");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Give me a another number.....");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int userResult = Class1.AddMethod(x, y);
+
+            Console.WriteLine(x + " * " + y + " = " + userResult);
+
+            //TAKES DECIMALS AND TURNS THEM INTO INTEGERS ===========
+
+            Console.WriteLine("Give me a decimal...");
+            decimal x1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Give me a another decimal.....");
+            decimal y2 = Convert.ToDecimal(Console.ReadLine());
+
+            int userResult2 = Class1.AddMethod(x, y);
+
+            Console.WriteLine(x1 + " + " + y2 + " = " + userResult2);
+
+            //Takes STRINGS INTO INTEgers ==========================
+            Console.WriteLine("Give me a number, lets turn it into a interger.");
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine("Give me another interger.");
+            string userInput2 = Console.ReadLine();
+
+            int stringAnswer = Class1.AddMethod(userInput, userInput2);
+
+            Console.WriteLine(stringAnswer);
+
+        }
+    }
+}
