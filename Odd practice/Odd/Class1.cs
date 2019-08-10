b716c4d52ac487df23c9ddcf77d8a16a9e4578bb
Zavier <zmyles@comcast.net>
2019-08-10 15:22:00 -0400

    Odd Practices Exercise

---
+++ Odd practice/Odd/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odd
+{
+    class OddClass { 
+    
+
+        //int num = 12;
+        //int num2 = 10;
+
+        public void AddInt(int num)
+        {
+            int result = num * 5;
+
+            Console.WriteLine(num2);
+            Console.ReadLine();
+
+            return;
+        }
+
+
+    }
+
+    
+}
---
+++ Odd practice/Odd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int userInput = OddClass.AddInt(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine(userInput);
+
+            
+
+
+
+        }
+    }
+}
