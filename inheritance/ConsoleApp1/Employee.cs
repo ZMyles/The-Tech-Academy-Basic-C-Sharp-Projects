601d0b944edd81ea7c3adf378f9d45a484cac32f
Zavier <zmyles@comcast.net>
2019-08-10 22:29:25 -0400

    inheritance exercise

---
+++ inheritance/ConsoleApp1/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Employee : Person
+    {
+        int Id;
+         SayName();
+    }
+}
---
+++ inheritance/ConsoleApp1/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+     public class Person
+    {
+
+        public string FirstName;
+        public string LastName;
+        public void SayName()
+        {
+            string fullName = FirstName + LastName;
+
+             Console.WriteLine("Name: [" + fullName + "]");
+            
+            
+        }
+        
+    }
+}
