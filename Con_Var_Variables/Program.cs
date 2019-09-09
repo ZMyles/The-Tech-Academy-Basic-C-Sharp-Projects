f7340a6ac062770398e962ec6a8f26e4a97cf478
Zavier <zmyles@comcast.net>
2019-09-09 14:48:23 -0400

    Var and const varibales exercise

---
+++ Con_Var_Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Con_Var_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Zavier";
+
+            var myLastName = "Myles";
+
+            Console.WriteLine("My name is {0} {1} and this is coding for dummbies.", myName, myLastName);
+            Console.ReadLine();
+        }
+    }
+}
