1a523c06bb3a2512dfa5726d9203dbd7302d0561
Zavier <zmyles@comcast.net>
2019-10-10 19:42:00 -0400

    Do vs Do While Updated

---
+++ Do_DoWhileLoops/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_DoWhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Do Loop ==========
+            Console.WriteLine("Thinking between 1 & 10...What's my number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool numberGuessed = number == 7;
+
+            do
+            {
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("Nope!");
+                        Console.WriteLine("Try again...");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("Not Even!");
+                        Console.WriteLine("Try again...");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 10:
+                        Console.WriteLine("You Even trying..?");
+                        Console.WriteLine("Try again...");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 6:
+                        Console.WriteLine("Almost...");
+                        Console.WriteLine("could be better.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 7:
+                        Console.WriteLine("Winner, winner!");
+                        numberGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Keep at it!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!numberGuessed);
+
+            Console.ReadLine();
+
+            //While Loop =================================
+
+            int i = 1;
+            while(i <= 10)
+            {
+                Console.WriteLine("I Value: {0}", i);
+                i++;
+            }
+            Console.WriteLine("Press Enter Key to Exit..");
+
+            Console.ReadLine();
+
+        }
+    }
+}
