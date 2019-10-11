9c8455ccfdbabf9618dc1e0ef66d691c1397d81c
Zavier <zmyles@comcast.net>
2019-10-11 17:11:00 -0400

    Updated File_I_O

--- FileIO/Program.cs
+++ FileIO/Program.cs
@@ -1,23 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 
-namespace File_I_O
+namespace FileIO
 {
     class Program
     {
         static void Main(string[] args)
         {
-         
             Console.WriteLine("Please enter any number.");
             string userInput = Console.ReadLine();
 
             File.WriteAllText(@"C:\Users\Zavier Myles\Desktop\Number_Storage\UserNumber.txt", userInput);
 
-            Console.WriteLine("Thanks for the: {0}" , userInput);
+            Console.WriteLine("Thanks for the: ", userInput);
             Console.ReadLine();
 
         }
