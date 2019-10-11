06c05a6deed2f2862acf1a58db4a3b214ee8b950
Zavier <zmyles@comcast.net>
2019-10-11 16:58:16 -0400

    Added Method class

---
+++ MultiMethod/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiMethod
+{
+    class Class1
+    {
+
+        public static int AdditonMethod(int j, string i)
+        {
+            //Code figure outs where they entered a number or didn't add anything
+            if (i == "")
+            {
+                int noInput = 10;
+                int result = j + noInput;
+                return result;
+            }
+            else
+            {
+                int realNumber = Convert.ToInt32(i);
+                int result = j + realNumber;
+                return result;
+            }
+        }
+    }
+ }
---
+++ MultiMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultiMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Gathers user Inputs
+            Console.WriteLine("Please enter a Number");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("If you wish, you can add another number. If not don't type anything");
+            string userInput2 = Console.ReadLine();
+
+            // Pushes Inputs int Method
+            int result = Class1.AdditonMethod(userInput1, userInput2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
