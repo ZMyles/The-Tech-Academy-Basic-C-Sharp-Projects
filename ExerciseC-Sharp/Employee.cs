016f66f65010f87ea568e972b72e863e71200b18
Zavier <zmyles@comcast.net>
2019-10-25 19:37:17 -0400

    Test commit

--- ExerciseC-Sharp/Employee.cs
+++ ExerciseC-Sharp/Employee.cs
@@ -6,14 +6,26 @@ using System.Threading.Tasks;
 
 namespace ExerciseC_Sharp
 {
-    public class Employee : Person 
+    public class Employee : Person, IQuittable
     {
+        public int ID { get; set; }
 
         public override void SayName()
         {
             base.SayName();
         }
 
+        public static bool operator == (Employee employee, Employee employee1)
+        {
+            bool reuslt = employee == employee1;
+            return reuslt;
+        }
+
+        public static bool operator != (Employee employee, Employee employee1)
+        {
+            bool reuslt = employee != employee1;
+            return reuslt;
+        }
 
         //public override void Test()
         //{
@@ -22,6 +34,13 @@ namespace ExerciseC_Sharp
 
         //IQUITTABLE ============================================
 
+        public void Quit()
+        {
+            Console.WriteLine("I quite...");
+        }
+
+
+       
 
     }
 }
--- ExerciseC-Sharp/Program.cs
+++ ExerciseC-Sharp/Program.cs
@@ -13,8 +13,24 @@ namespace ExerciseC_Sharp
             Employee employee = new Employee();
             employee.FirstName = "Sample";
             employee.LastName = "Student";
+            employee.ID = 25;
+
+            Employee employee1 = new Employee();
+            employee1.ID = 25;
 
             employee.SayName();
+
+            //IQuittable =====================================
+            IQuittable person = new Employee();
+            person.Quit();
+
+
+            if (employee.ID == employee1.ID)
+            {
+                Console.WriteLine("If he quits, Then I Quit!!");
+                Console.ReadLine();
+            }
+
         }
     }
 }
